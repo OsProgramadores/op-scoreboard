f1d6c2c9b336c94dc7122cafee4bfbffc2f3ebc7
Marco Paganini <paganini@paganini.net>
2018-12-15 19:17:10 -0800

    First commit.

---
+++ config.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"io"
+	"io/ioutil"
+)
+
+const (
+	configFile = "config.toml"
+
+	// Directory usually under $HOME/.config that holds all configurations.
+	botConfigDir = "pixiebot"
+)
+
+// Point holds the points required for a challenge.
+type Point struct {
+	Value int `toml:"value"`
+}
+
+// Config holds the main configuration items.
+type Config struct {
+	// Directory where osprogramadores/op-website-hugo is cloned.
+	WebsiteDir string `toml:"website_dir"`
+
+	// Directory where osprogramadores/op-desafios is cloned.
+	DesafiosDir string `toml:"desafios_dir"`
+
+	// Go Template directory.
+	TemplateDir string `toml:"template_dir"`
+
+	// points per desafio. This is a map where they key is taken from
+	// desafio-"nn" (currently a number but could be anything in the future)
+	// and the value is the number of points this desafio is worth.
+	Points map[string]Point `toml:"points"`
+
+	// Ignore these usernames (admins, and others that don't benefit
+	// from showing in the scoreboard).
+	IgnoreUsers []string `toml:"ignore_users"`
+}
+
+// parseConfig parses the configuration string from the slice of bytes
+// containing the TOML config read from disk and performs basic sanity checking
+// of configuration items.
+func parseConfig(r io.Reader) (Config, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config := Config{}
+	if _, err := toml.Decode(string(data), &config); err != nil {
+		return Config{}, err
+	}
+	fmt.Printf("Config is %+v\n", config)
+
+	// Check mandatory fields.
+	// if config.Username == "" || config.Password == "" || config.ClientID == "" || config.Secret == "" {
+	//	return botConfig{}, errors.New("usename/password/client_id/secret cannot be null")
+	//}
+
+	return config, nil
+}
---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	// git log --name-only --pretty=format: | grep '^desafio-'| cut -d/ -f1,2 |sort -u | cut -d/ -f2 | sort | uniq -c | sort
+	// -rn
+	// Glob matching desafios.
+	desafiosGlob = "./desafio-*/*"
+)
+
+// player holds all data from the contestants.
+type playerInfo struct {
+	fullName   string
+	githubUser string
+	avatarURL  string
+	points     int
+	challenges []string
+}
+
+// playerChallenge holds one user/challenge pair read from the disk.
+type playerChallenge struct {
+	username  string
+	challenge string
+}
+
+// playerScore holds scores for one particular player.
+type playerScore struct {
+	score int
+}
+
+func main() {
+	//log.SetFlags(0)
+	configFile := flag.String("config", "", "configuration file")
+	flag.Parse()
+
+	r, err := os.Open(*configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	config, err := parseConfig(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	challenges, err := readChallenges(config.DesafiosDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scores := map[string]playerScore{}
+
+	for _, c := range challenges {
+		// Make sure user is not ignored.
+		if inSlice(config.IgnoreUsers, c.username) {
+			continue
+		}
+
+		// Compute score for this player/challenge
+		pts, err := calcScores(c, config.Points)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, ok := scores[c.username]; !ok {
+			scores[c.username] = playerScore{score: 0}
+		}
+		s := scores[c.username]
+		s.score += pts
+		scores[c.username] = s
+	}
+
+	fmt.Printf("%+v\n", scores)
+}
+
+// readChallenges reads all relevant directories under ddir and
+// return a list containing the users and challenges found.
+func readChallenges(ddir string) ([]playerChallenge, error) {
+	var ret []playerChallenge
+
+	dpaths, err := filepath.Glob(filepath.Join(ddir, desafiosGlob))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range dpaths {
+		username, challenge, err := parsePath(v)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, playerChallenge{username: username, challenge: challenge})
+	}
+	return ret, nil
+}
+
+// parsePath parses a path under DesafiosDir and returns the user and
+// designation of that particular challenge (or error). This function assumes
+// that directories under path are laid out as desafio-NN/username
+func parsePath(path string) (string, string, error) {
+	elems := strings.Split(path, "/")
+	if len(elems) < 2 {
+		return "", "", fmt.Errorf("invalid file/dir: %q", path)
+	}
+
+	username := elems[len(elems)-1]
+	dpath := elems[len(elems)-2]
+
+	// Split desafio-NN
+	d := strings.Split(dpath, "-")
+	if len(d) != 2 {
+		return "", "", fmt.Errorf("invalid directory format: %q", dpath)
+	}
+	return username, d[1], nil
+}
+
+// calcScores returns the calcScores for a single username
+func calcScores(challenge playerChallenge, points map[string]Point) (int, error) {
+	pointvalue, ok := points[challenge.challenge]
+	if !ok {
+		return 0, fmt.Errorf("missing points configuration for: %q", challenge.challenge)
+	}
+	return pointvalue.Value, nil
+}
+
+func inSlice(sl []string, str string) bool {
+	for _, v := range sl {
+		if str == v {
+			return true
+		}
+	}
+	return false
+}
