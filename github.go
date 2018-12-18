ee44e2497fd687bcaa229b0d8a423fe4d5a9c242
Marco Paganini <paganini@paganini.net>
2018-12-17 21:58:39 -0800

    Added negative caching for 404s.

--- github.go
+++ github.go
@@ -56,21 +56,33 @@ type GithubUserResponse struct {
 // A boolean flag is returned to mean "user not found in github". All
 // other unexpected conditions return an error.
 func githubUserInfo(username string) (GithubUserResponse, bool, error) {
-	// Attempt to fetch json from cache first. Failing that,
-	// make a request directly to github.
-	jdata, ok, err := cached(username)
+	// Check the negative cache. This is for users who deleted their
+	// accounts on github (previously got a 404). Don't try these
+	// since these requests eat our freebie quota.
+	_, ok, err := cached(negativeCacheFile(username), maxAgeDays*24*time.Hour)
 	if err != nil {
-		return GithubUserResponse{}, false, fmt.Errorf("error retrieving cached data: %v", err)
+		return GithubUserResponse{}, false, fmt.Errorf("negative cache read error for %q: %v", username, err)
 	}
+	if ok {
+		return GithubUserResponse{}, false, nil
+	}
+
+	// Attempt to fetch json from cache first.
+	jdata, ok, err := cached(cacheFile(username), maxAgeDays*24*time.Hour)
+	if err != nil {
+		return GithubUserResponse{}, false, fmt.Errorf("cache read error for %q: %v", username, err)
+	}
+	// Not in cache. Fetch from github.
 	if !ok {
 		r, err := http.Get("https://api.github.com/users/" + username)
 		if err != nil {
-			return GithubUserResponse{}, false, fmt.Errorf("error retrieving github user: %v", err)
+			return GithubUserResponse{}, false, fmt.Errorf("error retrieving github user %q: %v", username, err)
 		}
-		// Ignore users that are not on github.
-		// TODO: Create a negative cache for those since they'll eat our
-		// freebie quota on github.
+		// Save username in our negative cache if we get a 404.
 		if r.StatusCode == http.StatusNotFound {
+			if err = cachesave(negativeCacheFile(username), []byte{}); err != nil {
+				return GithubUserResponse{}, false, fmt.Errorf("negative cache write error for %q: %v", username, err)
+			}
 			return GithubUserResponse{}, false, nil
 		}
 
@@ -79,33 +91,31 @@ func githubUserInfo(username string) (GithubUserResponse, bool, error) {
 		}
 		jdata, err = ioutil.ReadAll(r.Body)
 		if err != nil {
-			return GithubUserResponse{}, false, fmt.Errorf("error reading http body: %v", err)
+			return GithubUserResponse{}, false, fmt.Errorf("error reading http body for user %q: %v", username, err)
 		}
-
 	}
 	var resp GithubUserResponse
 	if err := json.Unmarshal(jdata, &resp); err != nil {
 		return GithubUserResponse{}, false, fmt.Errorf("error decoding github data: %v", err)
 	}
 
-	// Only save if we didn't read from the cache (to avoid resetting
-	// the file's timestamp.
+	// Only save if we didn't read from the cache.
+	// This avois prevents the unnecessary reset of the file's timestamp.
 	if !ok {
 		if err := cachesave(username, jdata); err != nil {
-			return GithubUserResponse{}, false, fmt.Errorf("error saving cache: %v", err)
+			return GithubUserResponse{}, false, fmt.Errorf("cache write error for %q: %v", username, err)
 		}
 	}
 
 	return resp, true, nil
 }
 
-// cached returns the data cached in a file (for a given username). A boolean
-// return indicates whether the cache exists and is valid (in which case, data
-// will contains valid data) or not.
-func cached(username string) ([]byte, bool, error) {
-	cfile := cachefile(username)
-
-	fi, err := os.Stat(cfile)
+// cached returns the data cached in a file. A duration specifies for how long
+// data in the cache is valid. Three values are returned: a slice of bytes
+// containing the data in the cache file (if considered valid), a boolean
+// indicating whether the data is valid or not (expired, etc), and an error.
+func cached(cachefile string, exp time.Duration) ([]byte, bool, error) {
+	fi, err := os.Stat(cachefile)
 	if err != nil {
 		// Return no error on not exists condition (use the boolean to signal).
 		if os.IsNotExist(err) {
@@ -114,11 +124,11 @@ func cached(username string) ([]byte, bool, error) {
 		return nil, false, err
 	}
 	// Is this file older than maxAgeDays?
-	if time.Now().After(fi.ModTime().Add(maxAgeDays * 24 * time.Hour)) {
+	if time.Now().After(fi.ModTime().Add(exp)) {
 		return nil, false, nil
 	}
 
-	data, err := ioutil.ReadFile(cfile)
+	data, err := ioutil.ReadFile(cachefile)
 	if err != nil {
 		return nil, false, err
 	}
@@ -126,24 +136,29 @@ func cached(username string) ([]byte, bool, error) {
 	return data, true, err
 }
 
-// cachesave saves cache data for a given username, creating the required
-// directory structure, if needed.
-func cachesave(username string, data []byte) error {
-	cfile := cachefile(username)
-	dir, _ := filepath.Split(cfile)
+// cachesave saves cache data into a file, creating the required directory
+// structure, if needed.
+func cachesave(cachefile string, data []byte) error {
+	dir, _ := filepath.Split(cachefile)
 
 	// Create the entire directory structure (if needed).
 	if err := os.MkdirAll(dir, 0777); err != nil {
 		return err
 	}
 
-	if err := ioutil.WriteFile(cfile, data, 0777); err != nil {
+	if err := ioutil.WriteFile(cachefile, data, 0777); err != nil {
 		return err
 	}
 	return nil
 }
 
-// cachefile returns the name of the cache file for a given user.
-func cachefile(username string) string {
+// cacheFile returns the name of the cache file for a given user.
+func cacheFile(username string) string {
 	return filepath.Join(cacheDir, username+".cache")
+}
+
+// negativeCachefile returns the name of the negative cache file for a given
+// user.
+func negativeCacheFile(username string) string {
+	return filepath.Join(cacheDir, username+".negcache")
 }
