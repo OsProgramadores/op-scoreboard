8df45aa247404a4e2794dbee2bbc8cf60c12f02f
Marco Paganini <paganini@paganini.net>
2018-12-17 22:33:55 -0800

    Reduced cyclomatic complexity.

--- github.go
+++ github.go
@@ -52,59 +52,92 @@ type GithubUserResponse struct {
 	UpdatedAt         time.Time   `json:"updated_at"`
 }
 
-// githubUserInfo returns github information about a given username.
-// A boolean flag is returned to mean "user not found in github". All
-// other unexpected conditions return an error.
-func githubUserInfo(username string) (GithubUserResponse, bool, error) {
+// readFromCache attempts to read data for a given username from the cache
+// files. First, an attempt is made at the negative cache files. If we have a
+// hit there, the user is considered as "non-existent". Otherwise, attempt to
+// read from the regular cache, obeying the usual expiration policy.  Returns
+// the data read from the cache file, a bool indicating whether the user data
+// was found (and is valid) and an error.
+func readFromCache(username string) ([]byte, bool, error) {
 	// Check the negative cache. This is for users who deleted their
 	// accounts on github (previously got a 404). Don't try these
 	// since these requests eat our freebie quota.
 	_, ok, err := cached(negativeCacheFile(username), maxAgeDays*24*time.Hour)
 	if err != nil {
-		return GithubUserResponse{}, false, fmt.Errorf("negative cache read error for %q: %v", username, err)
+		return nil, false, fmt.Errorf("negative cache read error for %q: %v", username, err)
 	}
+	// OK in the negative cache means we should not process it.
 	if ok {
-		return GithubUserResponse{}, false, nil
+		return nil, false, nil
 	}
 
-	// Attempt to fetch json from cache first.
+	// Attempt to fetch json from cache.
 	jdata, ok, err := cached(cacheFile(username), maxAgeDays*24*time.Hour)
 	if err != nil {
-		return GithubUserResponse{}, false, fmt.Errorf("cache read error for %q: %v", username, err)
+		return nil, false, fmt.Errorf("cache read error for %q: %v", username, err)
 	}
+	if !ok {
+		return nil, false, nil
+	}
+	return jdata, true, nil
+}
+
+// readFromGithub reads data from a user using the github API (v3). If a 404 is
+// returned, we save the user to the negative cache to avoid future accesses to
+// this user. Returns the data read from Github (json), a boolean indicating
+// whether we found a valid user or not, and an error.
+func readFromGithub(username string) ([]byte, bool, error) {
+	r, err := http.Get("https://api.github.com/users/" + username)
+	if err != nil {
+		return nil, false, fmt.Errorf("error retrieving github user %q: %v", username, err)
+	}
+	// Save username in our negative cache if we get a 404.
+	if r.StatusCode == http.StatusNotFound {
+		if err = cachesave(negativeCacheFile(username), []byte{}); err != nil {
+			return nil, false, fmt.Errorf("negative cache write error for %q: %v", username, err)
+		}
+		return nil, false, nil
+	}
+
+	if r.StatusCode < 200 || r.StatusCode > 299 {
+		return nil, false, fmt.Errorf("github returned status %d for user %q", r.StatusCode, username)
+	}
+	jdata, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, false, fmt.Errorf("error reading http body for user %q: %v", username, err)
+	}
+	return jdata, true, nil
+}
+
+// githubUserInfo returns github information about a given username.
+// A boolean flag is returned to mean "user not found in github". All
+// other unexpected conditions return an error.
+func githubUserInfo(username string) (GithubUserResponse, bool, error) {
+	// Attempt to read from cache.
+	jdata, ok, err := readFromCache(username)
+	if err != nil || !ok {
+		return GithubUserResponse{}, false, err
+	}
+
 	// Not in cache. Fetch from github.
 	if !ok {
-		r, err := http.Get("https://api.github.com/users/" + username)
-		if err != nil {
-			return GithubUserResponse{}, false, fmt.Errorf("error retrieving github user %q: %v", username, err)
-		}
-		// Save username in our negative cache if we get a 404.
-		if r.StatusCode == http.StatusNotFound {
-			if err = cachesave(negativeCacheFile(username), []byte{}); err != nil {
-				return GithubUserResponse{}, false, fmt.Errorf("negative cache write error for %q: %v", username, err)
-			}
-			return GithubUserResponse{}, false, nil
-		}
-
-		if r.StatusCode < 200 || r.StatusCode > 299 {
-			return GithubUserResponse{}, false, fmt.Errorf("github returned status %d for user %q", r.StatusCode, username)
-		}
-		jdata, err = ioutil.ReadAll(r.Body)
-		if err != nil {
-			return GithubUserResponse{}, false, fmt.Errorf("error reading http body for user %q: %v", username, err)
+		jdata, ok, err = readFromGithub(username)
+		if err != nil || !ok {
+			return GithubUserResponse{}, false, err
 		}
 	}
+
+	// Unmarshal the JSON and run very basic checks. If everything OK, save
+	// this to the cache.
 	var resp GithubUserResponse
 	if err := json.Unmarshal(jdata, &resp); err != nil {
 		return GithubUserResponse{}, false, fmt.Errorf("error decoding github data: %v", err)
 	}
-
-	// Only save if we didn't read from the cache.
-	// This avois prevents the unnecessary reset of the file's timestamp.
-	if !ok {
-		if err := cachesave(username, jdata); err != nil {
-			return GithubUserResponse{}, false, fmt.Errorf("cache write error for %q: %v", username, err)
-		}
+	if resp.Login == "" {
+		return GithubUserResponse{}, false, fmt.Errorf("got bad json from github: %s", string(jdata))
+	}
+	if err := cachesave(cacheFile(username), jdata); err != nil {
+		return GithubUserResponse{}, false, fmt.Errorf("cache write error for %q: %v", username, err)
 	}
 
 	return resp, true, nil
